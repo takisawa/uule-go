3df5fcd4178cdbd29407e76930c27250fffdcce6
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-03-10 19:23:58 +0900

    Add uule.Encode implementation.

--- uule-go.go
+++ uule-go.go
@@ -1,7 +1,16 @@
 package uule_go
 
+import (
+	"encoding/base64"
+	"fmt"
+	"unicode/utf8"
+)
+
+const SPECIAL_KEY_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+
 func Encode(location_canonical_name string) string {
-	return "Encode"
+	key := []rune(SPECIAL_KEY_TABLE)[utf8.RuneCountInString(location_canonical_name)]
+	return fmt.Sprintf("w+CAIQICI%s%s", string(key), base64.StdEncoding.EncodeToString([]byte(location_canonical_name)))
 }
 
 func Decode(uule string) string {
